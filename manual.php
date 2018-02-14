8da9e65b2300948ca4ad3c511100e071bc2b2d40
Andrew West <andrew@keyboardcowboy.co.uk>
2018-02-14 21:24:01 +0000

    - Added script to automate posting from the command line

---
+++ manual.php
@@ -0,0 +1,95 @@
+<?php
+
+if ( php_sapi_name() != "cli" ) {
+  print "This script must be run from the command line\n";
+  exit();
+}
+
+require_once("motouk.php");
+require_once("Phapper/src/phapper.php");
+include_once("config.php");
+
+function checkCurrentDailyCommute($reddit)
+{
+  echo "Checking for current thread\r\n";
+
+  $hasPost = false;
+
+  $today = new Datetime();
+  $today->setTime(0, 0, 0);
+
+  $result = $reddit->search('daily commute', 'motouk', null, 'day', null, 1);
+
+  $posts = $result->data->children;
+  foreach($posts AS $post)
+  {
+    $postDate = new DateTime('@'.$post->data->created_utc);
+    $postDate->setTime(0, 0, 0);
+
+    $hasPost = ($postDate == $today);
+    if ($hasPost)
+      break;
+  }
+
+  return $hasPost;
+}
+
+function postDailyCommute($reddit)
+{
+  $dailyCommute = new DailyCommute();
+
+  echo "Posting thread: {$dailyCommute->getTitle()}\r\n";
+
+  $content = <<<EOT
+# {$dailyCommute->getTitle()}
+
+Morning, looks like the regular daily commute posters are having a lie in, so this post has been automatically generated.
+
+Ride safe.
+
+### Forecast
+
+EOT;
+
+  foreach($dailyCommute->getWeather() AS $period)
+  {
+    $content .= "#### {$period['title']}\r\n";
+    $content .= "{$period['content']}\r\n";
+  }
+
+  $content .= <<<EOT
+
+### Sun times
+* {$dailyCommute->getSunset()}
+* {$dailyCommute->getSunrise()}
+
+### Discord
+{$dailyCommute->getDiscordLink()}
+
+### Bot Information
+This has been an automatic post. If you have any question, suggestions or bugs please message /u/namtabmai
+
+If you'd like to do tomorrow's daily commute thread, you can find an automatically generated template [here](http://www.keyboardcowboy.co.uk/motoukdailycommute/),
+you just need to replace the bits between the <> brackets.
+
+EOT;
+
+  $response = $reddit->submitTextPost(SUBREDDIT, $dailyCommute->getTitle(), $content, false);
+
+  var_dump($response);
+}
+
+$reddit = new Phapper(
+  Phapperconfig::$username,
+  Phapperconfig::$password,
+  PhapperConfig::$app_id,
+  PhapperConfig::$app_secret,
+  PhapperConfig::$user_agent
+);
+
+if (!checkCurrentDailyCommute($reddit))
+{
+  postDailyCommute($reddit);
+}
+
+?>
